ca207c4bf9be5750a7a8d1c39f0997fdb0213d34
TooBunReal <89735990+TooBunReal@users.noreply.github.com>
2023-06-19 21:25:22 +0700

    Add files via upload

---
+++ baitapAPi/baitapAPi/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace baitapAPi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bt_get = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(632, 371);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // bt_get
+            // 
+            this.bt_get.Location = new System.Drawing.Point(13, 13);
+            this.bt_get.Name = "bt_get";
+            this.bt_get.Size = new System.Drawing.Size(108, 48);
+            this.bt_get.TabIndex = 1;
+            this.bt_get.Text = "GET API";
+            this.bt_get.UseVisualStyleBackColor = true;
+            this.bt_get.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 450);
+            this.Controls.Add(this.bt_get);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bt_get;
+    }
+}
+
---
+++ baitapAPi/baitapAPi/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+namespace baitapAPi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Task
+        {
+            public int userId { get; set; }
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public bool Completed { get; set; }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://jsonplaceholder.typicode.com/todos";
+            var httpClient = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string response_data = await response.Content.ReadAsStringAsync();
+                var tasks = JsonConvert.DeserializeObject<Task[]>(response_data);
+                dataGridView1.DataSource = tasks;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
